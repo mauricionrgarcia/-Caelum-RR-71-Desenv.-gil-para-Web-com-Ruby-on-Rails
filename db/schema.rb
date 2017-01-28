e58d0e1c1eae9797801bb867deb62ecd39f5de2a
Mauricio N R Garcia <mauricionrgarcia@gmail.com>
2017-01-28 20:17:45 -0200

    criação da entidade e validações de Recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128220527) do
+ActiveRecord::Schema.define(version: 20170128220919) do
 
   create_table "clients", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       limit: 80
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20170128220527) do
     t.string   "name",       limit: 80
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+  end
+
+  create_table "recipes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "restaurants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
