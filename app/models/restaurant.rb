7fc988f4aebfb472eb8e3ff6929eb4341c4f6c50
Mauricio N R Garcia <mauricionrgarcia@gmail.com>
2017-01-28 19:56:18 -0200

    mg - implementação das validações na entidade Restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,17 @@
 class Restaurant < ApplicationRecord
+
+  validates_presence_of :name, message: "value can't be blank"
+  validates_presence_of :address, message: "value can't be blank"
+  validates_presence_of :specialty, message: "value can't be blank"
+
+  validates_uniqueness_of :name, message: 'name already registered'
+  validates_uniqueness_of :address, message: 'address already registered'
+
+  validate :first_char_should_be_uppercase
+
+  private
+  def first_char_should_be_uppercase
+    errors.add(:name, 'First character should be uppercase') unless name =~ /[A-Z].*/
+  end
+
 end
