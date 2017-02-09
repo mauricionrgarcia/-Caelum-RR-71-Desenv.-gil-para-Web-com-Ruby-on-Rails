0fb9b1298132bfdf531e76e851b58d5bdaf03873
Mauricio N R Garcia <mauricionrgarcia@gmail.com>
2017-02-08 22:09:53 -0200

    mg - chamando metodos de controle no VIEW - nao faça isso

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,10 +2,16 @@ class Client < ApplicationRecord
 
   has_many :qualifications
 
+  def to_s
+    name
+  end
+
   validates_presence_of :name, message: "value can't be blank"
   validates_uniqueness_of :name, message: 'name already registered'
-  validates_numericality_of :idade, greater_than: 0,
+  validates_numericality_of :years_old, greater_than: 0,
                                     less_than: 140,
                                     message: 'Must be a number between 0 and 100'
+
+
 
 end
